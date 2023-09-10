0ede865d75d04fc9462f3811eb264b2871c9e1ab
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-09-10 11:28:08 -0400

    another commit

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
-import Players from './Players.js'
+// import Players from './Players.js'
 
+class Players {
+	constructor(name, hand) {
+		this.name = name
+		this.hand = hand
+	}
+
+	set myHand(value) {
+		const [card1, card2] = value.split(" ")
+		this.hand = [card1, card2]
+	}
+}
+
+const player1 = new Players("Sean")
+
+player1.myHand = "AH 2S"
+
+function testHand(player) {
+	return player.hand[0]
+}
+
+console.log(testHand(player1))
+
+const communityCards = ["2H", "QS", "JD", "8C", "9H"]
+const playerCards = ["10H", "9D"]
+
+function parseCards(hand) {
+	const cards = hand.map((card) => {
+		const value = card.slice(0, -1)
+		const suit = card.slice(-1)
+		return { value, suit }
+	})
+
+	return cards
+}
+
+// const cards = communityCards.map((card) => {
+// 	const value = card.slice(0, -1)
+// 	const suit = card.slice(-1)
+// 	return { value, suit }
+// })
+
+console.log(parseCards(playerCards))
--- script.test.js
+++ script.test.js
@@ -1,9 +1,17 @@
-import { Players, testHand }  from "./Players"
+// import { Players, testHand }  from "./Players"
+
+// import Players from "./script.js"
+import { testHand } from "./script.js"
 
 describe("#check", () => {
+	// test("it tests", () => {
+	// 	const player1 = new Players("Sean")
+	// 	player1.myHand = "AH 2S"
+	// 	expect(testHand(player1.hand)).toBe("AH")
+	// })
+
 	test("it tests", () => {
-		const player1 = new Players("Sean")
-		player1.myHand = "AK 2S"
-		expect(testHand(player1.hand)).toBe("AK")
+		const value = 2
+		expect(testHand(value)).toBe(3)
 	})
 })
