43889b9910b8a49c08cd87da969158036c99ba5a
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-09-14 20:13:39 -0400

    refactored code

--- script.js
+++ script.js
@@ -20,75 +20,68 @@ function testHand(player) {
 	return player.hand[0]
 }
 
-const communityCards = ["2H", "QS", "JD", "8C", "9H"]
+const communityCards = ["2H", "8C", "9H", "JD", "QS", "10H", "9D"] //["0-2", "6-1","7-2", "9-0", "10-3" ] 2H, 8C, 9H, JD, QS
 const playerCards = ["10H", "9D"]
 
-function parseCards(hand) {
-	const cards = hand.map((card) => {
-		let value
-		const tempVal = card.slice(0, -1)
-
-		switch (tempVal) {
-			case "J":
-				value = 9
-				break
-			case "Q":
-				value = 10
-				break
-			case "K":
-				value = 11
-				break
-			case "A":
-				value = 12
-				break
-			default:
-				value = parseInt(card.slice(0, -1)) - 2
-		}
-
-		const suit = card.slice(-1)
-		return { value, suit }
+function parseCards(card) {
+	return card.map((card) => {
+		const valueMap = { J: 9, Q: 10, K: 11, A: 12 }
+		const value =
+			valueMap[card.slice(0, -1)] || parseInt(card.slice(0, -1)) - 2
+		const suitMap = { D: 0, C: 1, H: 2, S: 3 }
+		const suitCode = suitMap[card.slice(-1)]
+		return { value, suitCode }
 	})
-
-	return cards
 }
 
 const parsedComCards = parseCards(communityCards)
 
-function createMatrix(community) {
-	const matrix = []
-	for (let j = 0; j < 13; j++) {
-		const row = []
-		for (let i = 0; i < 4; i++) {
-			const element = {
-				value: i,
-				suit: j,
-			}
-
-			// community.forEach((card) => {
-			// 	if (card.value === j) {
-			// 		element = {
-			// 			value: true,
-			// 			suit: i,
-			// 		}
-			// 	} else {
-			// 		element = {
-			// 			value: false,
-			// 			suit: i,
-			// 		}
-			// 	}
-			// })
-
-			row.push(element)
-		}
-		matrix.push(row)
-	}
-	return matrix
+function checkPair(cards) {
+	let pair
 }
-const cardMatrix = parseCards(communityCards)
+
+// function createMatrix(community) {
+// 	const matrix = []
+// 	let element = {
+// 		value: undefined,
+// 		suit: undefined,
+// 		filled: undefined,
+// 	}
+// 	for (let j = 0; j < 13; j++) {
+// 		// value of the card
+// 		const row = []
+// 		for (let i = 0; i < 4; i++) {
+// 			// suit of the card
+// 			// ["0-2", "6-1","7-2", "9-0", "10-3" ]
+
+// 			element = {
+// 				value: j,
+// 				suit: i,
+// 				filled: false,
+// 			}
+
+// 			for (let index = 0; index < community.length; index++) {
+// 				if (
+// 					community[index].value === j &&
+// 					community[index].suitCode === i
+// 				) {
+// 					element.filled = true
+// 				}
+// 			}
+
+// 			matrix.push(element)
+// 		}
+// 	}
+// 	return matrix
+// }
 
 console.log("community cards:", parsedComCards)
-console.log("populated matrix:", cardMatrix)
-console.log("matrix:", createMatrix())
+const valueMap = { J: 9, Q: 10, K: 11, A: 12 }
+
+// const matrix = createMatrix(parsedComCards)
+// const reducedMatrix = matrix.filter((obj) => obj.filled !== false)
+
+// console.log(matrix[6])
 
 // parsedComCards.forEach((element) => {
 // 	console.log(element.value)
---
+++ test.js
@@ -0,0 +1,17 @@
+const communityCards = ["2H", "8C", "9H", "JD", "QS", "10H", "9D"] //["0-2", "6-1","7-2", "9-0", "10-3" ] 2H, 8C, 9H, JD, QS
+
+function parseCards(card) {
+	return card.map((card) => {
+		const valueMap = { J: 9, Q: 10, K:11, A:12 }
+		const value = valueMap[card.slice(0, -1)] || parseInt(card.slice(0, -1)) - 2
+    const suitMap = {D:0, C:1, H:2, S:3}
+    const suitCode = suitMap[card.slice(-1)]
+    return{value, suitCode}
+			
+	})
+}
+
+
+
+const valueMap = { J: 9, Q: 10, K:11, A:12 }
+console.log(valueMap["J"])
