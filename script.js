0fe47b21a31fd52857bf615e7671639f839e9f8f
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-09-16 17:30:28 -0400

    adding checking

--- script.js
+++ script.js
@@ -20,11 +20,13 @@ function testHand(player) {
 	return player.hand[0]
 }
 
-const communityCards = ["2H", "8C", "9H", "JD", "QS", "10H", "9D"] //["0-2", "6-1","7-2", "9-0", "10-3" ] 2H, 8C, 9H, JD, QS
+const communityCards = ["2H", "9C", "9H", "JD", "QS"] //["0-2", "6-1","7-2", "9-0", "10-3" ] 2H, 8C, 9H, JD, QS
 const playerCards = ["10H", "9D"]
 
+const fullCards = [...communityCards, ...playerCards]
+
 function parseCards(card) {
-	return card.map((card) => {
+	const parsedCards = card.map((card) => {
 		const valueMap = { J: 9, Q: 10, K: 11, A: 12 }
 		const value =
 			valueMap[card.slice(0, -1)] || parseInt(card.slice(0, -1)) - 2
@@ -32,13 +34,84 @@ function parseCards(card) {
 		const suitCode = suitMap[card.slice(-1)]
 		return { value, suitCode }
 	})
+
+	parsedCards.sort((a, b) => a.value - b.value)
+	return parsedCards
 }
 
-const parsedComCards = parseCards(communityCards)
+const parsedFullCards = parseCards(fullCards)
 
-function checkPair(cards) {
-	let pair
+function checkHand(array) {
+	let handValue = {
+		highCard: false,
+		onePair: false,
+		twoPair: false,
+		threeOfKind: false,
+		straight: false,
+		flush: false,
+		fullHouse: false,
+		fourOfKind: false,
+		straightFlush: false,
+		royalFlush: false,
+	}
+
+	// return findDuplicates(array, "value")
 }
+
+function checkCombos(array) {
+	// Each rank should display what pair, twoPair, etc. they actually are to compare against other players
+	let pair = {
+		value: undefined,
+		has: false,
+	}
+	let twoPair = {
+		value: undefined,
+		has: false,
+	}
+	let trip = {
+		value: undefined,
+		has: false,
+	}
+	let quad = {
+		value: undefined,
+		has: false,
+	}
+
+	const valueArray = array.map((card) => card.value)
+
+	const duplicates = []
+	const uniquValues = {}
+	for (const value of valueArray) {
+		if (uniquValues[value]) {
+			console.log(uniquValues[value])
+			duplicates.push(value)
+		} else {
+			uniquValues[value] = true
+		}
+	}
+	return { duplicates, uniquValues }
+}
+
+console.log(checkCombos(parsedFullCards))
+
+// function findDuplicates(array, property) {
+// 	const duplicates = []
+// 	const uniquValues = {}
+
+// 	for (const obj of array) {
+// 		const value = obj[property]
+// 		if (uniquValues[value]) {
+// 			duplicates.push(obj)
+// 		} else {
+// 			uniquValues[value] = true
+// 		}
+// 	}
+// 	return duplicates
+// }
+
+// console.log(checkHand(parsedFullCards))
+
+console.log("card array:", parsedFullCards)
 
 // function createMatrix(community) {
 // 	const matrix = []
@@ -74,9 +147,6 @@ function checkPair(cards) {
 // 	}
 // 	return matrix
 // }
-
-console.log("community cards:", parsedComCards)
-const valueMap = { J: 9, Q: 10, K: 11, A: 12 }
 
 // const matrix = createMatrix(parsedComCards)
 // const reducedMatrix = matrix.filter((obj) => obj.filled !== false)
